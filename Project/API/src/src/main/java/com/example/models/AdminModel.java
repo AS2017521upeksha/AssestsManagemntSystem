4c81db79d708ff6f8d6ad46eba1299be64ef7e45
RashmikaD2001 <rashmika.d2167@gmail.com>
2023-07-12 21:01:50 +0530

    Create model and repository classes of Admin

--- Project/API/src/src/main/java/com/example/models/AdminModel.java
+++ Project/API/src/src/main/java/com/example/models/AdminModel.java
@@ -1,5 +1,26 @@
 package com.example.models;
 
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import com.mongodb.lang.NonNull;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document(collection = "Admin")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AdminModel {
 
+    @Id
+    private ObjectId id;
+    private String username;
+    private String email;
+    private String role;
 }
