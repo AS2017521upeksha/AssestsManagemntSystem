0e261914fe1aaaecff4ae003c52f8a11aa4af6d2
binulsanthiya <binulsanthiya66@gmail.com>
2023-07-15 12:28:26 +0530

    Put Validation on EmbededAssetModel and Create Controller and Service class

--- Project/API/src/src/main/java/com/example/models/EmbededAssetModel.java
+++ Project/API/src/src/main/java/com/example/models/EmbededAssetModel.java
@@ -9,7 +9,10 @@ import com.mongodb.lang.NonNull;
 
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,9 +23,25 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 
 public class EmbededAssetModel {
+    
+    //Represant the ID of item
+    @NonNull
+    @Positive
     private Number item_id;
+
+    //Represant the name of item
+    @NotNull
+    @NotBlank
     private String item_name;
+
+    //Represant the quantity of item
+    @NonNull
+    @PositiveOrZero
     private Number quantity;
+
+    //Reprasant the code for requesting the item
+    @NonNull
+    @Positive
     private Number request_code;
 
 }
