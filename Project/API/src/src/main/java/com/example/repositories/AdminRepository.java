d35d3d397368c27c4a5e76090d54c67408a2a56e
RashmikaD2001 <rashmika.d2167@gmail.com>
2023-07-15 11:11:30 +0530

    Insert validations and comments

--- Project/API/src/src/main/java/com/example/repositories/AdminRepository.java
+++ Project/API/src/src/main/java/com/example/repositories/AdminRepository.java
@@ -1,5 +1,11 @@
 package com.example.repositories;
 
-public class AdminRepository {
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.example.models.AdminModel;
+
+public interface AdminRepository extends MongoRepository<AdminModel, ObjectId> {
 
 }
