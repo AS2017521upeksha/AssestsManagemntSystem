9401adb012cb593a50b896c0260dc73aeb7b85d5
Dhanushka Prabhath <dhanushkaprabhath2001@gmail.com>
2023-07-16 07:26:59 +0530

    create AssetService class

--- Project/API/src/src/main/java/com/example/repositories/AssetRepository.java
+++ Project/API/src/src/main/java/com/example/repositories/AssetRepository.java
@@ -1,10 +1,14 @@
 package com.example.repositories;
 
+import java.util.Optional;
+
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.example.models.AssetModel;
 
 public interface AssetRepository extends MongoRepository<AssetModel, ObjectId> {
+
+    Optional<AssetModel> findById(String id);
 
 }
---
+++ Project/API/src/src/main/java/com/example/services/AssetService.java
@@ -0,0 +1,68 @@
+package com.example.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.models.AssetModel;
+import com.example.repositories.AssetRepository;
+
+@Service
+public class AssetService {
+
+    @Autowired
+    // This code segment declares a private field named assetRepository1
+    private AssetRepository assetRepository1;
+
+    // This code segment retrieves a list of all AssetModel objects from the
+    // repository.
+    public List<AssetModel> assetModelList() {
+
+        List<AssetModel> assetModels = assetRepository1.findAll();
+        return assetModels;
+    }
+
+    // This code segment adds an AssetModel object to the repository.also works as
+    // create method
+    public AssetModel addAssetModel(AssetModel assetModel1) {
+        return assetRepository1.save(assetModel1);
+    }
+
+    // This code segment retrieves an AssetModel object from the repository based on
+    // its ID.
+    public AssetModel getAssetById(String id) {
+        return assetRepository1.findById(id).orElse(null);
+    }
+
+    // This code segment updates an AssetModel object in the repository based on its
+    // ID.
+    public AssetModel updateAsset(String id, AssetModel updatedAssetModel) {
+        AssetModel existingAssetModel = assetRepository1.findById(id).orElse(null);
+        if (existingAssetModel != null) {
+            // Update the existing assetModel object with the new values
+            existingAssetModel.setItem_code(updatedAssetModel.getItem_code());
+            existingAssetModel.setItem_name(updatedAssetModel.getItem_name());
+            existingAssetModel.setRequest_code(updatedAssetModel.getRequest_code());
+            existingAssetModel.setLocation(updatedAssetModel.getLocation());
+            existingAssetModel.setBrand(updatedAssetModel.getBrand());
+            existingAssetModel.setPurchase_date(updatedAssetModel.getPurchase_date());
+            existingAssetModel.setBarcode(updatedAssetModel.getBarcode());
+
+            return assetRepository1.save(existingAssetModel);
+        }
+        return null;
+    }
+
+    // This code segment deletes an AssetModel object from the repository based on
+    // its ID.
+    public boolean deleteAsset(String id) {
+        AssetModel existingAssetModel = assetRepository1.findById(id).orElse(null);
+        if (existingAssetModel != null) {
+            assetRepository1.delete(existingAssetModel);
+            return true;
+        }
+        return false;
+    }
+
+}
